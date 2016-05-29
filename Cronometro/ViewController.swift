04a7edd704422b862435570aaa29beaeee2091e1
iXavy <development@ixavy.com>
2016-05-29 10:47:46 +0200

    Stop y Play sin cambio de icono

--- Cronometro/ViewController.swift
+++ Cronometro/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = NSTimer()
+    
+    var count:Int = 0
+    
+    var runing:Bool = false
+    
+    @IBOutlet weak var time: UILabel!
+    
+    @IBAction func stop(sender: AnyObject) {
+        runing = false
+        timer.invalidate()
+        count = 0
+        time.text = "\(count)"
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        if runing {
+            runing = false
+            timer.invalidate()
+        } else {
+            runing = true
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.autoTime), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func autoTime() {
+        
+        count += 1
+        
+        time.text = "\(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+               // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
